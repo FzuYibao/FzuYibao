627bdbba842bd239a6dc5bdf7b16fce4c7b66259
Mike <272451865@qq.com>
2017-11-12 21:17:08 +0800

    2017.11.12

---
+++ app/app/src/main/java/com/maple27/fzuyibao/view/activity/LibraryActivity.java
@@ -0,0 +1,15 @@
+package com.maple27.fzuyibao.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.maple27.fzuyibao.R;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
